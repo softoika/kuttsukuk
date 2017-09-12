4c61fe80382c354adc8e302a654bd7e380013b0f
花房亮 <hanafusaryo@hanafusaryou-no-MacBook-Pro.local>
2017-09-12 14:54:04 +0900

    ブロックの挙動を戻した(ブロックの重さは変わらない)。+BlockDurability.scatterImpactは使わないので削除

--- Assets/_Scripts/BlockDurability.cs
+++ Assets/_Scripts/BlockDurability.cs
@@ -15,15 +15,6 @@ public class BlockDurability : MonoBehaviour
     private int durability = 10;
 
     [SerializeField]
-    private float initialMass = 1; // durability > 0のときの重さ
-
-    [SerializeField]
-    private float jointedMass = 0.0001f; // ボールにくっついているときの重さ
-
-    [SerializeField]
-    private float scatterImpact = 10;
-
-    [SerializeField]
     private Color hitColor = Color.red;
 
     [SerializeField]
@@ -33,7 +24,6 @@ public class BlockDurability : MonoBehaviour
     private int ballFeed = 1;
 
     private Rigidbody2D rig;
-    private bool stickable = false;
     private Color initialColor;
     private Renderer texture;
     private FixedJoint2D joint;
@@ -55,53 +45,46 @@ public class BlockDurability : MonoBehaviour
     {
         rig = GetComponent<Rigidbody2D>();
         rig.constraints = RigidbodyConstraints2D.FreezePosition;
+        rig.Sleep();
         texture = GetComponent<Renderer>();
         joint = GetComponent<FixedJoint2D>();
         initialColor = texture.material.color;
         GameObject ball = GameObject.FindWithTag("Ball");
         ballMass = ball.GetComponent<BallMass>();
-        rig.mass = initialMass;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ball" 
+        if (collision.gameObject.tag == "Ball"
             || collision.gameObject.tag == "BallSatellites")
         {
-            durability -= ballMass.CurrentFeed;
-            AudioPlayer.PlayNonOverwrapping(AudioManager.Instance.BlockBreak);
+
             if (durability > 0)
             {
+                durability -= ballMass.CurrentFeed;
+                AudioPlayer.PlayNonOverwrapping(AudioManager.Instance.BlockBreak);
                 StartCoroutine(ChangeColor());
-            }else if(durability <= 0 && joint.connectedBody == null){
+            }
+            else if (durability <= 0 && joint.connectedBody == null)
+            {
                 // ボールにくっつく
                 joint.connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
                 rig.constraints = RigidbodyConstraints2D.None;
                 gameObject.tag = "BallSatellites";
                 ballMass.Feed(ballFeed, gameObject);
-                // ブロックの重さを変える
-                rig.mass = jointedMass;
                 // ボールの自体の重さも増やしてバランスを取れるようにする
-                ballMass.AddMass(jointedMass);
+                ballMass.AddMass(rig.mass);
+                // TODO:くっつくとき用のSEに差し替える
+                AudioPlayer.PlayNonOverwrapping(AudioManager.Instance.BlockBreak);
             }
         }
 
     }
 
-    private IEnumerator ChangeColor(){
+    private IEnumerator ChangeColor()
+    {
         texture.material.color = hitColor;
         yield return new WaitForSeconds(shockInterval);
         texture.material.color = initialColor;
-    }
-
-    // ランダムな方向にブロックに力がかかる
-    private void Scatter(){
-        if (!stickable)
-        {
-            float rad = Random.Range(0.0f, 360.0f) / (Mathf.PI * 180);
-            Vector2 direction = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
-            rig.AddForce(direction * scatterImpact, ForceMode2D.Impulse);
-            stickable = true;
-        }
     }
 }
