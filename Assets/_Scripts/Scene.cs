5b19d311612ae25b5110afbc23db849c5ad935e4
花房亮 <zwgnz@gmail.com>
2017-09-13 20:32:27 +0900

    シーンの再読込を実装＋再読込ボタン

---
+++ Assets/_Scripts/Scene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            Restart();
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
